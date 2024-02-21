ca921e2a3b5f38e4afc9d3adbbd9ef14d3c1e10d
Heyu Fu <Herry Fu>
2024-02-21 16:36:00 +1100

    Add Finish function Timer function application finished

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Loader from "./components/Loader";
 import Error from "./components/Error";
 import StartScreen from "./components/StartScreen";
 import Question from "./components/Question";
+import NextButton from "./components/NextButton";
+import Progress from "./components/Progress";
+import FinishScreen from "./components/FinishScreen";
+import Timer from "./components/Timer";
 
 const initialState = {
   questions: [],
@@ -14,6 +18,8 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
+  secondsRemaining: 10,
 };
 
 function reducer(state, action) {
@@ -32,7 +38,8 @@ function reducer(state, action) {
       };
     case "start": return {
       ...state,
-      status: "active"
+      status: "active",
+      secondsRemaining: state.questions.length * 30,
     };
     case "newAnswer":
       const question = state.questions.at(state.index);
@@ -42,6 +49,24 @@ function reducer(state, action) {
         answer: action.payload,
         points: action.payload === question.correctOption ? state.points + question.points : state.points,
       };
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
+    case "finish":
+      return { ...state, status: "finished", highscore: state.points > state.highscore ? state.points : state.highscore };
+    case "restart":
+      return {
+        ...state,
+        status: "ready",
+        points: 0,
+        index: 0,
+        answer: null,
+        secondsRemaining: state.questions.length * 30,
+      }
+    case "tick": return {
+      ...state,
+      secondsRemaining: state.secondsRemaining - 1,
+      status: state.secondsRemaining < 0 ? "finished" : state.status,
+    }
     default:
       throw new Error("Action unknown");
   }
@@ -49,9 +74,10 @@ function reducer(state, action) {
 
 function App() {
 
-  const [{ questions, status, index, answer }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer, points, highscore, secondsRemaining }, dispatch] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce((prev, cur) => prev + cur.points, 0);
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
@@ -68,7 +94,17 @@ function App() {
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
         {status === "ready" && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
-        {status === "active" && <Question question={questions[index]} dispatch={dispatch} answer={answer} />}
+        {status === "active" &&
+          <>
+            <Progress index={index} numQuestions={numQuestions} points={points} maxPossiblePoints={maxPossiblePoints} answer={answer} />
+            <Question question={questions[index]} dispatch={dispatch} answer={answer} />
+            <footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+              <NextButton dispatch={dispatch} answer={answer} index={index} numQuestions={numQuestions} />
+            </footer>
+          </>
+        }
+        {status === "finished" && <FinishScreen points={points} maxPossiblePoints={maxPossiblePoints} highscore={highscore} dispatch={dispatch} />}
       </Main>
     </div>
   );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,6 @@
 function Main({ children }) {
     return (
         <main className="main">
-            <p>1/15</p>
-            <p>Question?</p>
             {children}
         </main>
     );
